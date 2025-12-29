3ada2f5c9339091b43e71bd115b2b9dd40869648
Kevin Caudell <kevin.caudell.1@gmail.com>
2025-12-29 01:03:40 -0500

    Added fight() and battle() functions

--- battle_engine.py
+++ battle_engine.py
@@ -1,5 +1,51 @@
 ### Imports ###
 
-import classes
+import classes as C
 
 ###############
+
+### Battle Sequence ###
+
+def fight(player, mob):
+    """Fight loop to enact the trade-off of attacks between mob and player."""
+    while True:
+        action = input(f"[A]ttack or [H]eal: ").strip().lower()
+        if action not in ('a', 'h'):
+            print('Invalid selection.')
+            continue
+        break
+
+    if action == 'a':
+        dmg = player.attack(mob)
+        mob.take_damage(dmg)
+    else:
+        player.heal()
+    
+    if not mob.isAlive():
+        return None
+
+    dmg = mob.attack(player)
+    player.take_damage(dmg)
+    
+
+def battle(player, mob):
+    """A battle loop for each individual encounter between a player and a mob.
+       Return True if mob was killed, False if player was killed."""
+    
+    mob_messages = {f"A {mob._name} has appeared!", f"You are approaching a {mob._name}"}
+    print(mob_messages[0])
+
+    while True:
+        fight(player,mob)
+        if not mob.isAlive():
+            print(f"{mob._name} has been slayed.")
+            return True
+        if not player.isAlive():
+            print(f"{player._name} has died.")
+            return False
+            
+
+
+
+
+
